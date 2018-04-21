72cb959d3c4c6b67b2d1c43e0fd68dcd8d2af4fb
Tyler Crill <Tyler.W.Crill@hsutx.edu>
2018-04-21 13:11:53 -0500

    Working sams class added

--- src/samsStore.java
+++ src/samsStore.java
@@ -2,6 +2,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,12 +43,11 @@ public class samsStore {
 
             System.out.println("**** " + url + " ****");
             Document doc = Jsoup.connect(url).userAgent("Chrome/65.0.3325.181").get();
-            Element divContent = doc.select("span.hubbleTrackRecord[data-price]").first();
-            //String elem = divContent.attr("aria-label");
-            System.out.println(divContent.text().replace("$",""));
-//            System.out.println(price);
-//            shoppingCart.put(str, price);
-//            totalCost +=price;
+            String divContentAttr = doc.select("span.hubbleTrackRecord.collapse").attr("data-price");
+            Double price = Double.parseDouble(divContentAttr);
+            //System.out.println("price = " + divContentAttr);
+            shoppingCart.put(str, price);
+            totalCost += price;
 
         }
 
